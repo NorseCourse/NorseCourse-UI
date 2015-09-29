924fe0c8a0c2acf74a4e34624468d79cf04b98cb
Grant Barnes <gfbarnes78@gmail.com>
2015-09-29 15:41:31 -0500

    update

--- src/app/norsecourse.controller.js
+++ src/app/norsecourse.controller.js
@@ -2,6 +2,8 @@
     'use strict';
     angular.module('norseCourse').controller('test',function($scope) {
         $scope.name = 'world';
+        $scope.dept = "Computer Science";
+        $scope.class = "Senior Project";
         $scope.chips = ['computer science','math','physics','biology'];
         //$scope.toggleSidenav = function(menuId) {
         //    $mdSidenav(menuId).toggle();
