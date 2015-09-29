58621ca3245a4c3a9b125036546bfa9ee9c0659a
Michael Moore <mscottmoore16@gmail.com>
2015-09-29 15:00:13 -0500

    Added angular material and material icon css and scripts

--- src/app/norsecourse.controller.js
+++ src/app/norsecourse.controller.js
@@ -2,5 +2,8 @@
     'use strict';
     angular.module('norseCourse').controller('test',function($scope) {
         $scope.name = 'world';
+        //$scope.toggleSidenav = function(menuId) {
+        //    $mdSidenav(menuId).toggle();
+        //};
     });
 })();
