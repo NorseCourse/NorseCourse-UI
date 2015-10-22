95af00754a75ce0bfc409b323b5371beb5a02f3b
Michael Moore <mscottmoore16@gmail.com>
2015-10-22 14:00:31 -0500

    Configured angular material palette

--- src/app/module.js
+++ src/app/module.js
@@ -1,5 +1,12 @@
 (function() {
     'use strict';
-    angular.module('norseCourse', ['ngMaterial']);
+    angular.module('norseCourse', ['ngMaterial'])
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('indigo')
+                .accentPalette('pink')
+                .warnPalette('red')
+                .backgroundPalette('grey');
+        });
 })();
 
