062a8e9d08622d4e776d6ab310c0336761d1de97
Michael Moore <mscottmoore16@gmail.com>
2015-11-08 18:24:03 -0600

    UI prototype for schedule planner

---
+++ src/app/schedule-planner.controller.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+    angular.module('norseCourse').controller('schedulePlannerController', function($scope, norseCourseService) {
+        $scope.expanded = 'form';
+        $scope.requiredCourses = [];
+        $scope.preferredCourses = [];
+        $scope.requiredGenEds = [];
+        $scope.preferredGenEds = [];
+
+        $scope.toggleExpanded = function(section) {
+            if ($scope.expanded === section) {
+                $scope.expanded = null;
+            } else {
+                $scope.expanded = section;
+            }
+        };
+        
+        $scope.courseQuery = function(queryText) {
+            var types = ['course'];
+            return norseCourseService.autocompleteQuery(queryText, types);
+        };
+
+        $scope.genEdQuery = function(queryText) {
+            var types = ['gen ed'];
+            return norseCourseService.autocompleteQuery(queryText, types);
+        };
+    });
+})();
