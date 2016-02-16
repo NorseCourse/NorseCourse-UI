55739c55fadadadd2a11a30fc14707f00783b56c
Michael Moore <mscottmoore16@gmail.com>
2016-02-16 11:20:58 -0600

    documentation for schedulesService

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -11,6 +11,15 @@ NG_DOCS={
       "moduleName": "norseCourse",
       "shortDescription": "Contains methods and models to allow users to find courses",
       "keywords": "$scope allow api apply array autocomplete autocompleted autocompletequery autocompletes based contents controller course courses coursesearchterms current departments display displays doesn eds empty erases find findcourses finds function gen keywords method methods model models newkeyword newvalue norsecourse oldvalue optional options querytext search side string terms text updated user users work"
+    },
+    {
+      "section": "api",
+      "id": "norseCourse.service:schedulesService",
+      "shortName": "schedulesService",
+      "type": "service",
+      "moduleName": "norseCourse",
+      "shortDescription": "Service for syncing schedule preferrences across the NorseCourse app",
+      "keywords": "add addpreferredcourse addpreferredgened addrequiredcourse addrequiredgened adds api app array check checks course courses eds gen gened getpreferredcourses getpreferredgeneds getrequiredcourses getrequiredgeneds hascourse hasgened haspreferredcourse haspreferredgened hasrequiredcourse hasrequiredgened list method norsecourse preferred preferrences remove removecourse removed removegened removepreferredcourse removepreferredgened removerequiredcourse removerequiredgened removes required schedule service syncing true"
     }
   ],
   "apis": {
