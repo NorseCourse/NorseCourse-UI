70e77f09c704a5eabb4af0fce6e6c6c1eb8ff208
Michael Moore <mscottmoore16@gmail.com>
2016-04-05 15:17:17 -0500

    Ng-routing for tabs; closes #43

--- src/app/module.js
+++ src/app/module.js
@@ -1,12 +1,22 @@
 (function() {
     'use strict';
-    angular.module('norseCourse', ['ngMaterial','ngMdIcons'])
-        .config(function($mdThemingProvider) {
+    angular.module('norseCourse', ['ngRoute', 'ngMaterial','ngMdIcons'])
+        .config(function($mdThemingProvider, $routeProvider) {
             $mdThemingProvider.theme('default')
                 .primaryPalette('indigo')
                 .accentPalette('pink')
                 .warnPalette('red')
                 .backgroundPalette('grey');
+
+            $routeProvider.when('/find', {
+                resolve: {
+                    index: function() { return 0; }
+                }
+            }).when('/plan', {
+                resolve: {
+                    index: function() { return 1; }
+                }
+            }).otherwise('/find');
         });
 })();
 
