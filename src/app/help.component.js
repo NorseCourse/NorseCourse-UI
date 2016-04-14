16041d74b315f31ba8504b6a7899933a1ef238a2
Michael Moore <mscottmoore16@gmail.com>
2016-04-13 22:06:28 -0500

    Little hack to get the youtube showing at better sizes and ratios for all browsers

--- src/app/help.component.js
+++ src/app/help.component.js
@@ -6,6 +6,11 @@
      * @name norseCourse.directive:ncHelp
      */
     angular.module('norseCourse').component('ncHelp', {
+        controller: function($scope) {
+            $scope.getWidth = function(selector) {
+                return $(selector).width();
+            };
+        },
         templateUrl: 'views/app/help.html'
     });
 })();
